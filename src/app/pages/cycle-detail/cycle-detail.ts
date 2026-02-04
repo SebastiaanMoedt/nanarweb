1446da5dcf53fdbb76ddda41336424a89d18962b
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-04 11:30:49 +0100

    cycle detail update with getbyinfo & random film

--- src/app/pages/cycle-detail/cycle-detail.ts
+++ src/app/pages/cycle-detail/cycle-detail.ts
@@ -20,10 +20,10 @@ export class CycleDetail implements OnInit{
   listeFilms!: Film[]
 
 ngOnInit(): void {
-    this.listeFilms = this.getCycle(this.cycleName);
+    this.listeFilms = this.getCycleSortedByYear(this.cycleName);
 }
 
-getCycle(name: string): Film[]{
+getCycleSortedByYear(name: string): Film[]{
   let allFilms: Film[] = this.cycleDbMock.getAllFilmsInCycle(name)
   let sortedFilms = allFilms.sort((a, b) => {
     if (a.year < b.year) {
@@ -35,69 +35,28 @@ getCycle(name: string): Film[]{
     return 0;
   });
   return sortedFilms
-}}
+}
 
 
-// import { Directive, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
-// import { DecimalPipe } from '@angular/common';
+getCycleSortedBySpecialInfo(name: string): Film[]{
+  let allFilms: Film[] = this.cycleDbMock.getAllFilmsInCycle(name)
+  let sortedFilms = allFilms.sort((a, b) => {
+    if (a.specialInfo < b.specialInfo) {
+        return -1;
+    }
+    if (a.specialInfo > b.specialInfo) {
+        return 1;
+    }
+    return 0;
+  });
+  return sortedFilms
+}
 
-// export type SortColumn = keyof Film | '';
-// export type SortDirection = 'asc' | 'desc' | '';
-// const rotate: { [key: string]: SortDirection } = { asc: 'desc', desc: '', '': 'asc' };
+getRandomFilmFromCycle(name: string): Film{
+  let allFilms: Film[] = this.cycleDbMock.getAllFilmsInCycle(name)
+  let randomFilm = allFilms[Math.floor(Math.random() * allFilms.length)]
+  return randomFilm
 
-// const compare = (v1: string | number, v2: string | number) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
+}
 
-// export interface SortEvent {
-// 	column: SortColumn;
-// 	direction: SortDirection;
-// }
-
-// @Directive({
-// 	selector: 'th[sortable]',
-// 	host: {
-// 		'[class.asc]': 'direction === "asc"',
-// 		'[class.desc]': 'direction === "desc"',
-// 		'(click)': 'rotate()',
-// 	},
-// })
-// export class NgbdSortableHeader {
-// 	@Input() sortable: SortColumn = '';
-// 	@Input() direction: SortDirection = '';
-// 	@Output() sort = new EventEmitter<SortEvent>();
-
-// 	rotate() {
-// 		this.direction = rotate[this.direction];
-// 		this.sort.emit({ column: this.sortable, direction: this.direction });
-// 	}
-// }
-
-// @Component({
-// 	selector: 'ngbd-table-sortable',
-// 	imports: [DecimalPipe, NgbdSortableHeader],
-//   templateUrl: './cycle-detail.html',
-//   styleUrl: './cycle-detail.css',
-// })
-// export class NgbdTableSortable {
-//   sortedFilms = listeFilms
-
-// 	@ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
-
-// 	onSort({ column, direction }: SortEvent) {
-// 		// resetting other headers
-// 		for (const header of this.headers) {
-// 			if (header.sortable !== column) {
-// 				header.direction = '';
-// 			}
-// 		}
-
-// 		// sorting countries
-// 		if (direction === '' || column === '') {
-// 			this.sortedFilms = listeFilms;
-// 		} else {
-// 			this.sortedFilms = [...listeFilms].sort((a, b) => {
-// 				const res = compare(a[column], b[column]);
-// 				return direction === 'asc' ? res : -res;
-// 			});
-// 		}
-// 	}
-// }
+}
