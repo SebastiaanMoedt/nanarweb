603f3a2db3ee082f71828122a08c2ae4f9902348
SebastiaanMoedt <eveningdreamer@proton.me>
2026-02-13 11:28:25 +0100

    lexend added

--- src/app/pages/cycles/cycles.ts
+++ src/app/pages/cycles/cycles.ts
@@ -16,6 +16,48 @@ export class Cycles {
   cycleDetail = new CycleDetail()
   cyclesServices = new CyclesService()
   private router = inject(Router);
+  gifs = [
+    'gifs/bride1.gif',
+    'gifs/dancing1.gif',
+    'gifs/dancing2.gif',
+    'gifs/dino1.gif',
+    'gifs/dove1.gif',
+    'gifs/dracula1.gif',
+    'gifs/dvd1.gif',
+    'gifs/dvd2.gif',
+    'gifs/emo1.gif',
+    'gifs/eye1.gif',
+    'gifs/eye2.gif',
+    'gifs/eye3.gif',
+    'gifs/eye4.gif',
+    'gifs/frankenstein1.gif',
+    'gifs/ghost1.gif',
+    'gifs/godzilla1.gif',
+    'gifs/godzilla2.gif',
+    'gifs/halloween1.gif',
+    'gifs/halloween2.gif',
+    'gifs/hellcome1.gif',
+    'gifs/hellraiser1.gif',
+    'gifs/hellraiser2.gif',
+    'gifs/house1.gif',
+    'gifs/mermaid1.gif',
+    'gifs/mermaid2.gif',
+    'gifs/monster1.gif',
+    'gifs/movie1.gif',
+    'gifs/planet1.gif',
+    'gifs/planet2.gif',
+    'gifs/shark1.gif',
+    'gifs/shark2.gif',
+    'gifs/skeleton1.gif',
+    'gifs/skull1.gif',
+    'gifs/skull2.gif',
+    'gifs/skull3.gif',
+    'gifs/tritri1.gif',
+    'gifs/trumpet1.gif',
+    'gifs/ufo1.gif',
+    'gifs/ufo2.gif',
+    'gifs/whale1.gif'
+  ]
 
   getAllCycles(){
     return this.cyclesServices.getAllCycles()
@@ -41,6 +83,11 @@ export class Cycles {
   getCycleLength(name: string): number{
     return this.cyclesServices.getCycleLength(name)
   }
+
+randomImg(): string{
+  const randomIndex = Math.floor(Math.random() * this.gifs.length - 1)
+  return this.gifs[randomIndex]
+}
 
 navigateToCycleDetail(name : string){
   this.cycleDetail.cycleName = name
